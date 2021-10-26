296391348f83d9a06b956903175a3d6ae1a11003
Rigi99 <64904615+Rigi99@users.noreply.github.com>
2021-10-26 15:18:02 +0300

    26.10

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 # apiSecurity = 'wQb4Gyh9zxM2GZ03Xx8Ldok40tVjhO1KG1RBjYdoegbG67r3ogt2qeDocCDwGhIV'
 # # Zsombi
 
-# apiKey = 'k9PP6Q9Zop25GDns7iDwopDx9ERmqmgp7tokaNlVCmNT5nk9hQvzXU0Q7DzOOvca'
-# apiSecurity = 'CaX9mtvJUpytOQeTL5WxkOsz7qyYyF6Q2sKAvAHBkRnehcVn9QOekfmvQH6bSqo5'
+apiKey = 'k9PP6Q9Zop25GDns7iDwopDx9ERmqmgp7tokaNlVCmNT5nk9hQvzXU0Q7DzOOvca'
+apiSecurity = 'CaX9mtvJUpytOQeTL5WxkOsz7qyYyF6Q2sKAvAHBkRnehcVn9QOekfmvQH6bSqo5'
 # Zsolt
