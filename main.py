bd8c9686f21ee488e8c91b743ae19c4f315b4f4d
Rigi99 <64904615+Rigi99@users.noreply.github.com>
2021-10-27 11:25:30 +0300

    27.10 Coinbase data

--- main.py
+++ main.py
@@ -7,25 +7,48 @@ from binance import BinanceSocketManager
 import time
 from binance import ThreadedWebsocketManager
 import matplotlib.pyplot as plt
+import websocket
+import json
 
 
-def handle_socket_message(msg):
-    df = pd.DataFrame([msg])
-    df = df.loc[:, ['s', 'E', 'p']]
-    df.columns = ['symbol', 'Time', 'Price']
-    df.Price = df.Price.astype(float)
-    df.Time = pd.to_datetime(df.Time, unit='ms')
-    df.to_sql('BTCUSDT', engine, if_exists='append', index=False)
-    print(df)
+def on_open(ws):
+    print('The socket is open!')
+    subscribe_message = {
+        'type': 'subscribe',
+        'channels': [{
+            'name': 'ticker',
+            'product_ids': ['BTC-USD']
+        }
+        ]
+    }
+    ws.send(json.dumps(subscribe_message))
+
+
+def on_message(ws, message):
+    current_data = json.loads(message)
+    print(current_data)
+
+
+# def handle_socket_message(msg):
+#     df = pd.DataFrame([msg])
+#     df = df.loc[:, ['s', 'E', 'p']]
+#     df.columns = ['symbol', 'Time', 'Price']
+#     df.Price = df.Price.astype(float)
+#     df.Time = pd.to_datetime(df.Time, unit='ms')
+#     df.to_sql('BTCUSDT', engine, if_exists='append', index=False)
+#     print(df)
 
 
 if __name__ == '__main__':
-    symbol = 'BTCUSDT'
-    twm = ThreadedWebsocketManager(config.apiKey, config.apiSecurity)
-    twm.start()
-    twm.start_trade_socket(callback=handle_socket_message, symbol=symbol)
-    engine = sqlalchemy.create_engine('sqlite:///BTCUSDstream.db')
-    aux = pd.read_sql('BTCUSDT', engine)
-    print(aux)
-    plt.plot(aux.Price)
-    plt.show()
+    # symbol = 'BTCUSDT'
+    # twm = ThreadedWebsocketManager(config.apiKey, config.apiSecurity)
+    # twm.start()
+    # twm.start_trade_socket(callback=handle_socket_message, symbol=symbol)
+    # engine = sqlalchemy.create_engine('sqlite:///BTCUSDstream.db')
+    # aux = pd.read_sql('BTCUSDT', engine)
+    # print(aux)
+    # plt.plot(aux.Price)
+    # plt.show()
+    socket = 'wss://ws-feed.exchange.coinbase.com'
+    ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message)
+    ws.run_forever()
