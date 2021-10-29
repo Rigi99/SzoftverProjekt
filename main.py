61e532523551abff78ab36db53332bdd6754660e
zakariasZs <zakarias.zsolt@student.ms.sapientia.ro>
2021-10-29 08:56:09 +0300

    (ZZs) Mean Reversion simulation on binance data.

--- main.py
+++ main.py
@@ -65,3 +65,70 @@ if __name__ == '__main__':
     # aux = pd.read_sql('Coin', engine)
     # print(aux)
     print('Hello')
+
+# Mean Reversion simulation
+# import pandas as pd
+# # import pandas_datareader as pdr
+# from binance.client import Client
+# from binance import ThreadedWebsocketManager
+# from binance import BinanceSocketManager
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from matplotlib import rcParams
+# rcParams['figure.figsize'] = 8,6
+# import seaborn as sb
+# import time
+# import config
+# import sqlalchemy
+# from datetime import datetime
+# sb.set()
+#
+#
+# def handle_socket_message(msg):
+#     df = pd.DataFrame([msg])
+#     df = df.loc[:, ['s', 'E', 'p']]
+#     df.columns = ['symbol', 'Time', 'Price']
+#     df.Price = df.Price.astype(float)
+#     df.Time = pd.to_datetime(df.Time, unit='ms')
+#     df.to_sql('BTCUSDT', engine, if_exists='append', index=False)
+#     print(df)
+#     dateTimeObj = datetime.now()
+#     print('System ts:',dateTimeObj)
+#
+#     # create moving average and add it to our data frame
+#     ma = 200
+#     btc['ma'] = btc['Price'].rolling(window=ma, min_periods=ma).mean()
+#
+#     # moving average=determine the direction of a trend, it is continually recalculated based on the latest price data
+#     macol = float(btc['ma'][-1:])
+#     print('Moving average:',macol)
+#     # closing price= the last price at which a stock trades during regular trading session
+#     close = float(btc['Price'][-1:])
+#     print('Close:',close)
+#
+#     # define the conditions we will make trades upon
+#     # We will want to buy if closing price is greater than the moving average
+#     if close > macol:
+#         print('Buy')
+#     elif close < macol:
+#         print('Sell')
+#     else:
+#         print('Not Trade')
+#     print('\n')
+#
+#     time.sleep(60)
+#
+# if __name__ == '__main__':
+#     symbol = 'BTCUSDT'
+#     twm = ThreadedWebsocketManager(config.apiKey, config.apiSecurity)
+#     twm.start()
+#     twm.start_trade_socket(callback=handle_socket_message, symbol=symbol)
+#     engine = sqlalchemy.create_engine('sqlite:///BTCUSDstream.db')
+#     btc = pd.read_sql('BTCUSDT', engine)
+#     plt.plot(btc.Price)
+#     plt.title('BTC Price Chart')
+#     plt.show()
+#
+#     # client data request
+#     # client= Client(config.apiKey, config.apiSecurity)
+#     # info=client.get_account()
