a1a35fb6a3cd83fca60a83bf248ee2ab7429158d
Rigi99 <64904615+Rigi99@users.noreply.github.com>
2021-11-19 10:03:12 +0200

    19.11

--- binanceData.py
+++ binanceData.py
@@ -44,7 +44,7 @@ def getHistoricalData1Day(Db, eng):
 
 def getHistoricalDataAux(Db, eng):
     client = Client(config.apiKey, config.apiSecurity)
-    klines = client.get_historical_klines(symbol='BTCBUSD', interval=Client.KLINE_INTERVAL_1MINUTE, start_str='18 Nov, 2021')
+    klines = client.get_historical_klines(symbol='BTCBUSD', interval=Client.KLINE_INTERVAL_1MINUTE, start_str=datetime.strftime(datetime.today(), '%d %B, %Y'))
     df = pd.DataFrame(klines)
     df = df.loc[:, [0, 1]]
     df.columns = ['DAY', 'Price']
@@ -61,15 +61,22 @@ def deleteDataBase(Db):
         print("The file does not exist")
 
 
-def getClientData():
+def getClientData(symbolCoin, symbolMoney):
     # clientAsset = Client(config.apiKey, config.apiSecurity)
     # data = clientAsset.get_account()
     # for it in data['balances']:
     #     print(it)
+    coinAmount = 0
+    baseBalance = 0
     with open('data.json', 'r') as file:
         data = json.load(file)
     for it in data['balances']:
-        print(it)
+        # print(it['asset'], it['free'])
+        if it['asset'] == symbolCoin:
+            coinAmount = float(it['free'])
+        if it['asset'] == symbolMoney:
+            baseBalance = float(it['free'])
+    return coinAmount, baseBalance
     # print(clientAsset.get_asset_balance('USDT'))
 
 
@@ -96,19 +103,44 @@ def movingAverageMethodBinance30Days(Db, eng):
 
 
 def run():
-    #alaptokenel ha kevesebb lenne eladas utan ne adjon el + kommentelni
-    # distanceListSell = [-1]
-    # distanceListBuy = [-1]
-    # if currentPrice >= ma30Days && ma7Days >= ma30Days:
-    #     distanceListSell.append(math.sqrt(abs(ma7Days ** 2 - ma30Days ** 2)))
-    #     if distanceListSell[-2] < distanceListSell[-1]:
-    #         f = open('Sell.txt', 'a')
-    #         f.write("Sell" + '\t\t' + str(currentPrice) + '\t\t' + str(datetime.now()))
-    #         f.close()
-    # elif currentPrice < ma30Days && ma7Days < ma30Days:
-    #     distanceListBuy.append(math.sqrt(abs(ma1Day ** 2 - ma30Days ** 2)))
-    #     if distanceListBuy[-2] < distanceListBuy[-1]:
-    #         f = open('Buy.txt', 'a')
-    #         f.write("Buy" + '\t\t' + str(currentPrice) + '\t\t' + str(datetime.now()))
-    #         f.close()
-    pass
+    coinBalance, baseBalance = getClientData(symbolCoin='BTC', symbolMoney='BUSD')
+    currentBalance = baseBalance
+    getHistoricalData1Day(config.DbHistorical, config.engineHistorical)
+    # aux = pd.read_sql(config.DbHistorical, config.engineHistorical)
+    # print(aux)
+    while True:
+        # ma1Day, currentPrice = movingAverageMethodBinance1Day(config.DbHistorical, config.engineHistorical)
+        # ma7Days, currentPrice = movingAverageMethodBinance7Days(config.DbHistorical, config.engineHistorical)
+        # ma30Days, currentPrice = movingAverageMethodBinance30Days(config.DbHistorical, config.engineHistorical)
+        # print(str(ma1Day) + '\t\t' + str(currentPrice))
+        # print(str(ma7Days) + '\t\t' + str(currentPrice))
+        # print(str(ma30Days) + '\t\t' + str(currentPrice))
+        ma1Day, currentPrice = 60000, 60000
+        ma7Days, currentPrice = 59000, 60000
+        ma30Days, currentPrice = 58500, 60000
+        distanceListSell = [-1]
+        distanceListBuy = [-1]
+        if currentPrice >= ma30Days and ma7Days >= ma30Days and coinBalance != 0:
+            distanceListSell.append(math.sqrt(abs(ma7Days ** 2 - ma30Days ** 2)))
+            if distanceListSell[-2] < distanceListSell[-1]:
+                if baseBalance < currentPrice * coinBalance:
+                    f = open('Sell.txt', 'a')
+                    currentBalance = coinBalance * currentPrice
+                    coinBalance = 0
+                    f.write("Sell" + '\t\t' + str(currentPrice) + '\t\t' + str(datetime.now()) + '\t\t' + str(currentBalance) + '\n')
+                    f.close()
+        elif currentPrice < ma30Days and ma7Days < ma30Days and currentBalance != 0:
+            distanceListBuy.append(math.sqrt(abs(ma1Day ** 2 - ma30Days ** 2)))
+            if distanceListBuy[-2] < distanceListBuy[-1]:
+                f = open('Buy.txt', 'a')
+                coinBalance = currentBalance / currentPrice
+                currentBalance = 0
+                f.write("Buy" + '\t\t' + str(currentPrice) + '\t\t' + str(datetime.now()) + '\t\t' + str(coinBalance) + '\n')
+                f.close()
+        deleteDataBase(config.DbHistorical + '.db')
+        time.sleep(65)
+        getHistoricalData1Day(config.DbHistorical, config.engineHistorical)
+        # coinBalance, baseBalance = getClientData(symbolCoin='BTC', symbolMoney='BUSD')
+        # aux = pd.read_sql(config.DbHistorical, config.engineHistorical)
+        # print(aux)
+    deleteDataBase(config.DbHistorical + '.db')
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 import binanceData
 import config
 import time
@@ -8,23 +10,6 @@ if __name__ == '__main__':
     # coinbaseData.movingAverageMethodCoinBase()
     # aux = pandas.read_sql(config.DbHistorical, config.engineHistorical)
     # print(aux)
-    i = 1
-    binanceData.getHistoricalData1Day(config.DbHistorical, config.engineHistorical)
-    aux = pandas.read_sql(config.DbHistorical, config.engineHistorical)
-    print(aux)
-    while i % 3 != 0:
-        ma1Day, currentPrice1 = binanceData.movingAverageMethodBinance1Day(config.DbHistorical, config.engineHistorical)
-        ma7Days, currentPrice7 = binanceData.movingAverageMethodBinance7Days(config.DbHistorical, config.engineHistorical)
-        ma30Days, currentPrice30 = binanceData.movingAverageMethodBinance30Days(config.DbHistorical, config.engineHistorical)
-        print(str(ma1Day) + '\t\t' + str(currentPrice1))
-        print(str(ma7Days) + '\t\t' + str(currentPrice7))
-        print(str(ma30Days) + '\t\t' + str(currentPrice30))
-        binanceData.deleteDataBase(config.DbHistorical + '.db')
-        time.sleep(65)
-        i = i + 1
-        binanceData.getHistoricalData1Day(config.DbHistorical, config.engineHistorical)
-        aux = pandas.read_sql(config.DbHistorical, config.engineHistorical)
-        print(aux)
-    binanceData.deleteDataBase(config.DbHistorical + '.db')
+    binanceData.run()
     # binanceData.deleteDataBase(config.DbRealTime+'.db')
     # binanceData.run()
